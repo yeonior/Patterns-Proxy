d76510d3f363cd187c699bb580ae74bd1af1498b
Rus <de.mr44@gmail.com>
2022-01-15 19:10:59 +0500

    Added UI

---
+++ Patterns-Proxy/DetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailViewController.swift
+//  Patterns-Proxy
+//
+//  Created by Ruslan on 15.01.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func updatePhotoButtonDidTapped(_ sender: UIButton) {
+        
+    }
+}
