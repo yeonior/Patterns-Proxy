1526b2a1423e76c7571c23e2eeda05fa93838e1b
Rus <de.mr44@gmail.com>
2022-01-15 22:14:39 +0500

    Added Proxy

---
+++ Patterns-Proxy/Proxy.swift
@@ -0,0 +1,30 @@
+//
+//  Proxy.swift
+//  Patterns-Proxy
+//
+//  Created by Ruslan on 15.01.2022.
+//
+
+import Foundation
+
+var cacheData: Data?
+
+class Proxy: LoadServiceProtocol {
+    
+    private let service: LoadServiceProtocol
+    
+    init(service: LoadServiceProtocol) {
+        self.service = service
+    }
+    
+    func loadPhoto(url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        if cacheData != nil {
+            service.loadPhoto(url: url, completion: { data, response, error in
+                cacheData = data
+                completion(data, response, error)
+            })
+        } else {
+            completion(cacheData, nil, nil)
+        }
+    }
+}
